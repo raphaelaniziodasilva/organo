57dff1eb7897b7242d0963d8a2c3a9f2b1984fbe
RaphaelAnizioSilva <raphael.silva@anjunexpress.com.br>
2023-07-27 16:20:40 -0300

    Função ao deletar

--- src/App.js
+++ src/App.js
@@ -197,7 +197,9 @@ function App() {
   return (
     <div>
       <Banner />
+
       <Formulario times={times.map(time => time.nome)} aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} />
+      
       <section className="times">
         <h1>Minha organização</h1>
         {times.map((time, indice) => <Time key={indice} time={time} colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} />)}
