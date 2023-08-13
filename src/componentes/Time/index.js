c2e8c711e167755bf44cc20da40dacece324af10
Raphael Anizio da Silva <raphaelanizio01@gmail.com>
2023-08-13 18:50:56 -0300

    favoritando os colaboradores

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -2,7 +2,7 @@ import Colaborador from '../Colaborador'
 import './time.css'
 import hexToRgba from 'hex-to-rgba';
 
-const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor, aoFavoritar }) => {
     return (
         colaboradores.length > 0 && <section className='time' 
         style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6')  }}
@@ -29,6 +29,7 @@ const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
                     colaborador={colaborador} 
                     corDeFundo={time.cor}
                     aoDeletar={aoDeletar}
+                    aoFavoritar={aoFavoritar}
                 />
                 })}
             </div>
